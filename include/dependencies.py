3a7e990e52d97673fcd9247c93226c4c6ce46334
Disa Mhembere <dmhembe1@jhu.edu>
2016-11-28 03:48:48 -0500

    deps: parser for python complete. Able to read a code dir, write deps, read deps from file

--- include/dependencies.py
+++ include/dependencies.py
@@ -28,79 +28,229 @@ import argparse
 import os
 import json
 
-from common import isdir
+from common import ls_r
+from common import get_ext
 import exceptions
+from os.path import dirname
 
-
+# Inline comment
 comment = {
-    ".py": "#", ".r": "#", ".j": "#", ".jl": "#",
-    ".c": "//", ".cpp": "//", ".java": "//",
-    ".mat": "%"
+        ".py": "#", ".ipynb":"#", ".r": "#", ".j": "#", ".jl": "#",
+        ".c": "//", ".cpp": "//", ".java": "//",
+        ".mat": "%"
     }
 
-supported_filetypes = comment.keys()
+supported_fileexts = comment.keys()
+supported_fileexts.append(".json") # read made deps
 
+# NOTE: We use " " as a placeholder for languages with no multi-line comment
+#   since all lines are stripped
 ml_comment = {
-    ".py": "'''", ".py": '"""',
-    ".j": "#=", ".jl": "=#",
-    ".c": "/*", ".cpp": "/*", ".java": "/*"
+        ".py": "'''", ".ipynb": '"""',
+        ".j": "#=", ".jl": "=#",
+        ".r": " ",
+        ".c": "/*", ".cpp": "/*", ".java": "/*"
     }
 
+# Closing multi-line comment
 close_ml_comment = {
-    ".py": '"""',
-    ".j": "=#", ".jl": "=#",
-    ".c": "*/", ".cpp": "*/", ".java": "*/"
+        ".py": '"""', ".ipynb": '"""',
+        ".j": "=#", ".jl": "=#",
+        ".r": " ",
+        ".c": "*/", ".cpp": "*/", ".java": "*/"
     }
+
+module_keywords = {
+        ".py": ["import", "from"], ".ipynb": ["import", "from"],
+        ".j": ["using", "import", "importall"],
+        ".r": ["source"],
+        ".c": ["#include"], ".cpp": ["#include"],
+        "java": ["import"],
+
+        }
 
 class DependParser(object):
+    def __init__(self, fileext):
+        """
+        @param fileext: The file extensions that we will inspect for deps
+        """
+        assert isinstance(fileext, list), "Accepted file extensions must be a "
+        "list"
+        self.fileext = fileext
+        self.__map__ = {} # key: is file, v: list(all files that depend on the file)
 
-    def __init__(self):
-        pass
-
-    def readcode(self, path, outfn, fileext=None):
+    def readcode(self, path):
         """
         @param path: The dir in which the code resides
-        @param outfn: The path/name of dep file
         """
 
-        for fsobj in os.walk(path):
-            if isdir(fsobj):
-                self.readcode(fsobj, outfn) # Don't overflow stack!
+        files = ls_r(path, self.fileext)
+        for ext in self.fileext:
+            if ext not in supported_fileexts:
+                raise exceptions.UnsupportedFileException(ext)
+
+        # TODO: ||ize
+        for fn in files:
+            print "Reading {} ...".format(fn)
+            self.read(fn)
+
+    def __put_dep__(self, mod, importer):
+        if self.__map__.has_key(mod):
+            self.__map__[mod].append(importer)
+        else:
+            self.__map__[mod] = [importer]
+
+    def __build_map__(self, fn, modlines):
+        """
+        Determine if import is local, if it is: add it as depenedency
+        """
+        if get_ext(fn).startswith(".py"):
+            self.__python_build_map__(fn, modlines)
+        else:
+            raise NotImplementedError("File type {} not yet "
+                    "supported".format(get_ext(fn)))
+
+    def __python_build_map__(self, fn, modlines):
+        """
+        Python has 3 patters for includes:
+        1. import module[.nested-module]
+        2. from module[.nested-module].script import function
+        3. from module[.nested-module] import script
+        """
+        base_dir = dirname(fn)
+        files = ls_r(base_dir, self.fileext)
+
+        for line in modlines:
+            split_line = line.split()
+            # 1. & 2.
+            mod = os.path.join(base_dir, *split_line[1].split("."))+".py"
+            if not (os.path.isfile(mod)) and line.startswith("from"):
+                # 3.
+                if len(split_line) != 4:
+                    raise exceptions.ParsingException("Cannot part 'from' "
+                            "import: {} in {}. Please conform to PEP8 import "
+                            "conventions".format(line, fn))
+
+                mod = os.path.join(base_dir,
+                        *(split_line[1]+"."+split_line[3]).split("."))+".py"
+            if not (os.path.isfile(mod)): continue
+            # else
+            self.__put_dep__(mod, fn)
+
+    def __json_build_map__(self, fn):
+        with open(fn, "rb") as f:
+            if len(self.__map__) == 0:
+                self.__map__ = json.load(f)
             else:
-                if fileext is None:
-                    fileext = os.path.splitext(fsobj).strip().lower()
-                    if fileext not in supported_filetypes():
-                        raise exceptions.UnsupportedFileException(fileext)
-                self.read(fsobj, filetype=fileext)
+                ondisk = json.load(f)
+                for k, v in ondisk.iteritems():
+                    if self.__map__.has_key(k): # Case we have the key
+                        for deps in v: # v is a list
+                            if deps not in self.__map__[k]:
+                                self.__map__[k].append(deps)
+                    else: # Case we don't have the key
+                        self.__map__[k] = v
 
-    def read(fn, filetype):
-        comm = comment(filetype)
-        ml_comm = ml_comment(filetype)
-        ml_cl_comm = close_ml_comment(filetype)
-        raise NotImplementedError("readfile")
+    def __r_build_map__(self):
+        raise NotImplementedError("R read")
 
-    def py_read(self):
-        raise NotImplementedError("py_read")
-
-    def r_read(self):
-        raise NotImplementedError("cpp_read")
-
-    def julia_read(self):
-        raise NotImplementedError("java_read")
+    def __julia_build_map__(self):
+        raise NotImplementedError("julia read")
 
     def read_deps(self):
         # read the dependency graph from disk
         raise NotImplementedError("read_deps")
 
+
+    def __check_local_mod__(self, fn, dep_encoding_lines):
+        pass
+
+    def read(self, fn):
+        """
+        @brief: Generic code reader to extract deps from a file. NOTE: The
+            file extension is very important as it determines A LOT!
+        @param fn: The filename to be read
+        """
+        ext = get_ext(fn)
+        if ext not in supported_fileexts:
+            raise NotImplementedError("Cannot read file type "
+                    "{} yet".format(ext))
+
+        if ext == ".json":
+            self.__json_build_map__(fn)
+            return
+
+        self.comm = comment[ext]
+        self.ml_comm = ml_comment[ext]
+        self.ml_cl_comm = close_ml_comment[ext]
+
+        with open(fn, "rb") as f:
+            dep_encoding_lines = []
+
+            ml_comm_flag = False
+            lineno = -1
+            while True:
+                lineno += 1
+                line = f.readline().strip()
+                if not line: break
+                if line.startswith(self.comm):
+                    continue
+                elif not ml_comm_flag and line.startswith(self.ml_comm):
+                    ml_comm_flag = True
+                    continue
+                elif line.startswith(self.ml_cl_comm) or \
+                        line.endswith(self.ml_cl_comm):
+                    if not ml_comm_flag: raise ParsingException("Multiline "
+                        "comment closing block without opening block line {}: "
+                            "{}".format(lineno, fn))
+                    ml_comm_flag = False
+                # look for import statements
+                for kywd in module_keywords[ext]:
+                    if line.startswith(kywd): dep_encoding_lines.append(line)
+
+            if (dep_encoding_lines): # Ignore empty dep files
+                self.__build_map__(fn, dep_encoding_lines)
+
+    def write_deps(self, outfn):
+        """
+        @param outfn: The path/name of dep fileext
+        """
+        with open(outfn, "wb") as f:
+            json.dump(self.__map__, f)
+
+    def __repr__(self):
+        return str(self.__map__)
+
+    def __eq__(self, other):
+        return self.__map__ == other.__map__
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+def test(path, outputfn, fileext):
+        dp = DependParser(fileext)
+        dp.readcode(path)
+        dp.write_deps(outputfn)
+
+        outdp = DependParser(fileext)
+        outdp.read(outputfn)
+        assert dp == outdp
+
 def main():
   parser = argparse.ArgumentParser(description="Dependency builder outputs a "
-          "file given a dir structure with all (local) non-package deps")
-  parser.add_argument("dir", action="store", help="The directory containing "
-          "files for which we want to build a dep graph")
+          "dir structure with all (local) non-package deps")
+  parser.add_argument("path", action="store", help="The file or a directory "
+          "containing files for which we want to build a dep graph")
+  parser.add_argument("fileext", action="store", help="The file "
+          "type we should parse for deps (others ignored) when given a "
+          "directory", nargs="+")
   parser.add_argument("-o", "--outputfn", action="store", help="The output "
-          "filename for the dep file written to disk")
+          "filename for the dep file written to disk", default="test_deps.json")
 
-  result = parser.parse_args()
+ # TODO: make ignore files arg
+  args = parser.parse_args()
+
+  test(args.path, args.outputfn, args.fileext)
 
 if __name__ == "__main__":
   main()
